f32f8c814904c2ab7ec0d9ad4f67cc76d7da3215
Steve Barham <dev@ethx.net>
2015-04-18 09:51:25 +0100

    Name constants consistently

--- src/test/java/net/ethx/shuteye/uri/UriTemplateTest.java
+++ src/test/java/net/ethx/shuteye/uri/UriTemplateTest.java
@@ -10,56 +10,56 @@ public class UriTemplateTest {
     @Test(expected = TemplateException.class)
     public void insufficientArgumentsVarArgs() {
         UriTemplateContext.defaultContext()
-                          .with(UriTemplateOption.AllowMissingVariablesForVarArg, Boolean.FALSE)
+                          .with(UriTemplateOption.ALLOW_MISSING_VARS_IN_VARARG, Boolean.FALSE)
                           .process("{foo}{bar}{baz}", 1, 2);
     }
 
     @Test(expected = TemplateException.class)
     public void tooManyArgumentsVarArgs() {
         UriTemplateContext.defaultContext()
-                          .with(UriTemplateOption.AllowExtraVariablesForVarArg, Boolean.FALSE)
+                          .with(UriTemplateOption.ALLOW_EXTRA_VARS_IN_VARARG, Boolean.FALSE)
                           .process("{foo}{bar}{baz}", 1, 2, 3, 4);
     }
 
     @Test
     public void insufficientArgumentsVarArgsAllowed() {
         assertEquals("12", UriTemplateContext.defaultContext()
-                                             .with(UriTemplateOption.AllowMissingVariablesForVarArg, true)
+                                             .with(UriTemplateOption.ALLOW_MISSING_VARS_IN_VARARG, true)
                                              .process("{foo}{bar}{baz}", 1, 2));
     }
 
     @Test
     public void tooManyArgumentsVarArgsAllowed() {
         assertEquals("123", UriTemplateContext.defaultContext()
-                                              .with(UriTemplateOption.AllowExtraVariablesForVarArg, true)
+                                              .with(UriTemplateOption.ALLOW_EXTRA_VARS_IN_VARARG, true)
                                               .process("{foo}{bar}{baz}", 1, 2, 3, 4));
     }
 
     @Test(expected = TemplateException.class)
     public void insufficientArgumentsMap() {
         UriTemplateContext.defaultContext()
-                          .with(UriTemplateOption.AllowMissingVariablesForMap, Boolean.FALSE)
+                          .with(UriTemplateOption.ALLOW_MISSING_VARS_IN_MAP, Boolean.FALSE)
                           .process("{foo}{bar}{baz}", Vars.wrap(ImmutableMap.of("foo", 1, "bar", 2)));
     }
 
     @Test(expected = TemplateException.class)
     public void tooManyArgumentsMap() {
         UriTemplateContext.defaultContext()
-                          .with(UriTemplateOption.AllowExtraVariablesForMap, Boolean.FALSE)
+                          .with(UriTemplateOption.ALLOW_EXTRA_VARS_IN_MAP, Boolean.FALSE)
                           .process("{foo}{bar}{baz}", Vars.wrap(ImmutableMap.of("foo", 1, "bar", 2, "baz", 3, "qux", 4)));
     }
 
     @Test
     public void insufficientArgumentsMapAllowed() {
         assertEquals("12", UriTemplateContext.defaultContext()
-                                             .with(UriTemplateOption.AllowMissingVariablesForMap, true)
+                                             .with(UriTemplateOption.ALLOW_MISSING_VARS_IN_MAP, true)
                                              .process("{foo}{bar}{baz}", Vars.wrap(ImmutableMap.of("foo", 1, "bar", 2))));
     }
 
     @Test
     public void tooManyArgumentsMapAllowed() {
         assertEquals("123", UriTemplateContext.defaultContext()
-                                              .with(UriTemplateOption.AllowExtraVariablesForMap, true)
+                                              .with(UriTemplateOption.ALLOW_EXTRA_VARS_IN_MAP, true)
                                               .process("{foo}{bar}{baz}", Vars.wrap(ImmutableMap.of("foo", 1, "bar", 2, "baz", 3, "qux", 4))));
     }
 }
